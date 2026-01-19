b97708252640b1a9d13c9b005b6fcc8787517254
woojin <woojin.email@gmail.com>
2026-01-19 23:07:20 +0000

    Update config.py

--- config.py
+++ config.py
@@ -149,8 +149,8 @@ def load_fonts():
     header = get_font(_config.get("font_header", "Nintendo-DS-BIOS.ttf"), 13)
 
     # CJK fonts (Galmuri)
-    cjk_main = get_font("Galmuri7.ttf", 9)
-    cjk_header = get_font("Galmuri9.ttf", 11)
+    cjk_main = get_font("Galmuri7.ttf", 8)
+    cjk_header = get_font("Galmuri9.ttf", 10)
 
     icons_path = base_path / _config.get("font_icons", "Icons.ttf")
     icons = ImageFont.truetype(str(icons_path), 6, layout_engine=ImageFont.Layout.BASIC) if icons_path.exists() else None
