d23bdcf08be9bd85489b83de62116ed926fd5546
woojin <woojin.email@gmail.com>
2026-01-27 02:59:52 +0000

    fix

--- scripts/pisugar/toggle_play.sh
+++ scripts/pisugar/toggle_play.sh
@@ -2,9 +2,11 @@
 # PiSugar 3 single-tap: Toggle play/pause
 # Configure in PiSugar web interface or /etc/pisugar-server/config.json
 
-# Find the user running paperjam and their D-Bus session
+# Find the user running paperjam and send D-Bus command as that user
 for uid in $(ls /run/user/ 2>/dev/null); do
-    export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$uid/bus"
-    dbus-send --print-reply --dest=org.mpris.MediaPlayer2.paperjam \
+    user=$(getent passwd "$uid" | cut -d: -f1)
+    [ -z "$user" ] && continue
+    runuser -u "$user" -- env DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$uid/bus" \
+        dbus-send --print-reply --dest=org.mpris.MediaPlayer2.paperjam \
         /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause 2>/dev/null && exit 0
 done
